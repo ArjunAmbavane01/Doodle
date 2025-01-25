0a7252c3f5949149f08c42707274522d20b5c432
Arjun <arjun.ambavane@gmail.com>
2025-01-25 21:55:16 +0530

    Fix signup,signin and room endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { compare, hash } from "bcrypt";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import {SignupSchema} from "@repo/common/zodSchema";
-import {prismaClient} from "@repo/db/client";
+import { SigninSchema, SignupSchema } from "@repo/common/zodSchema";
+import { prismaClient } from "@repo/db/client";
+import { auth } from "./middleware";
 
 const PORT = 3002;
 const app = express();
@@ -14,53 +16,82 @@ app.post("/", (req: Request, res: Response) => {
   res.send("hello");
 });
 
-app.post("/signup", (req: Request, res: Response) => {
-  const data = SignupSchema.safeParse(req.body);
-  if(!data.success){
-    res.status(400).json({
-      type:'error',
-      msg:'Invalid Input',
-      err:data
-    })
+app.post("/signup", async (req: Request, res: Response) => {
+  const parsedData = SignupSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(411).json({
+      message: "Validation error",
+      errors: parsedData.error.errors.map((err) => ({
+        path: err.path,
+        message: err.message,
+      })),
+    });
     return;
   }
-  console.log(req);
+
   try {
-    const conn = prismaClient.$connect();
-    const id = 1; // Replace with actual user creation logic
-    const token = jwt.sign({ userId: id }, JWT_SECRET);
+    const { email, username, password } = parsedData.data;
+    const hashedPassword = await hash(password, 10);
+    const user = await prismaClient.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
     res.status(200).json({
       type: "success",
       msg: "Signup successful",
       data: { token },
     });
-    return;
   } catch (e) {
     res.status(500).json({
       type: "error",
       msg: "Signup failed",
       error: e,
     });
-    return;
   }
 });
 
-app.post("/signin", (req: Request, res: Response) => {
-  const { username, password } = req.body;
+app.post("/signin", async (req: Request, res: Response) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(411).json({
+      message: "Validation error",
+      errors: parsedData.error.errors.map((err) => ({
+        path: err.path,
+        message: err.message,
+      })),
+    });
+    return;
+  }
+  const {email,password} = parsedData.data;
   try {
-    const user = { id: "1", username: "arjun", password: "12" }; // Replace with actual user fetching logic
-    if (user && user.password === password) {
-      const token = jwt.sign({ userId: user.id }, JWT_SECRET);
-      res.status(200).json({
-        type: "success",
-        msg: "Signin successful",
-        data: { token },
-      });
-      return;
+    const user = await prismaClient.user.findFirst({
+      where: { email:email },
+    });
+    if (user) {
+      const passCompare = await compare(password, user.password);
+      if (passCompare) {
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+        res.status(200).json({
+          type: "success",
+          msg: "Signin successful",
+          data: { token },
+        });
+        return;
+      } else {
+        res.status(403).json({
+          type: "error",
+          msg: "Incorrect Password",
+        });
+        return;
+      }
     }
     res.status(403).json({
       type: "error",
-      msg: "Invalid credentials",
+      msg: "user does not exist",
     });
     return;
   } catch (e) {
@@ -72,12 +103,32 @@ app.post("/signin", (req: Request, res: Response) => {
   }
 });
 
-// app.get('/room', ((req, res) => {
-//     const { username, password } = req.body;
-//     = user.create({ username, password });
-//     const token =
-
-// }))
+app.post("/room",auth, async (req:Request, res:Response) => {
+  const { name } = req.body;
+  // @ts-ignore
+  const userId = req.userId;
+  console.log(userId + " " + name)
+  try {
+    const room = await prismaClient.room.create({
+      data: {
+        slug: name,
+        adminId: userId,
+      },
+    });
+    console.log(room)
+    res.status(200).json({
+      type: "success",
+      msg: "Room created",
+      data: { roomId:room.id },
+    });
+  } catch (e) {
+    res.status(500).json({
+      type: "error",
+      msg: "Room creation failed",
+      error: e,
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -3,28 +3,33 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req?.header("Authoriaztion");
+  const authHeader = req?.header("Authorization");
   const token =
     authHeader && authHeader.startsWith("Bearer ")
       ? authHeader.split(" ")[1]
-      : "";
+      : null;
 
-  if (token) {
-    try {
-      const payload = jwt.verify(token, JWT_SECRET);
-      // req.userId = payload.userId.toString();
-      next();
-    } catch (e: unknown) {
-      res.status(500).json({
-        type: "error",
-        msg: "Invalid or expired token",
-        error: e,
-      });
-      return;
-    }
+  if (!token) {
+    res.status(401).json({
+      type: "error",
+      msg: "Token not present",
+    });
+    return;
   }
-  res.status(401).json({
-    type: "error",
-    msg: "Token not present",
-  });
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    console.log(payload)
+    // @ts-ignore
+    req.userId = payload.userId.toString();
+    // @ts-ignore
+    console.log(req.userId)
+    next();
+  } catch (e: unknown) {
+    res.status(500).json({
+      type: "error",
+      msg: "Invalid or expired token",
+      error: e,
+    });
+    return;
+  }
 };
